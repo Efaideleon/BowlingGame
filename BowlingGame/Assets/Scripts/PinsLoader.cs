4bcf5efe717370609568a8c0ac07d85b0d2e4739
Efaideleon <efaideleon@gmail.com>
2024-07-18 14:27:16 -0700

    dynamic pin loader

--- BowlingGame/Assets/Scripts/PinsLoader.cs
+++ BowlingGame/Assets/Scripts/PinsLoader.cs
@@ -1,42 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PinsLoader : MonoBehaviour
 {
     [SerializeField] GameObject bowlingPinPrefab;
-    private readonly static float pinsHeight = 1.2f;
-    private readonly Vector3[][] pinsPositions = new Vector3[][] {
-        new Vector3[] {
-            new(-3f, pinsHeight, 0),
-            new(-1f, pinsHeight, 0),
-            new(1f, pinsHeight, 0),
-            new(3f, pinsHeight, 0)
-        },
-        new Vector3[] {
-            new(-2, pinsHeight, -1),
-            new(0, pinsHeight, -1),
-            new(2, pinsHeight, -1)
-        },
-        new Vector3[] {
-            new(-1, pinsHeight, -2),
-            new(1, pinsHeight, -2)
-        },
-        new Vector3[] {
-            new(0, pinsHeight, -3)
-        }
-    };
+    [SerializeField] float spacing = 1;
+    private readonly static float heighLevel = 1.2f;
+    private readonly static Vector2 origin = new Vector2(0, 0);
+    private List<List<Vector3>> positions; 
 
     void Start()
     {
-        if (bowlingPinPrefab != null) {
-            foreach (Vector3[] row in pinsPositions)
+        positions = CalculatePositions();
+        if (bowlingPinPrefab != null)
+        {
+            foreach (List<Vector3> row in positions)
             {
                 foreach (Vector3 position in row)
                 {
                     Instantiate(bowlingPinPrefab, position, Quaternion.identity);
                 }
             }
-        } else {
+        }
+        else
+        {
             Debug.LogError("Error: The bawlingPinPrefab has not been assigned!");
         }
+    }
+
+    List<List<Vector3>> CalculatePositions()
+    {
+        List<List<Vector3>> positions = new List<List<Vector3>> ();
+        float spacingOffset;
+        float yPosition = origin.y;
+        float xPosition = origin.x;
+        float xStart = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            List<Vector3> row = new List<Vector3>();
+            for (int j = 0; j < i + 1; j++)
+            {
+                row.Add(new Vector3(xPosition, heighLevel, yPosition));
+                xPosition += spacing;
+            }
+            positions.Add(row);
+            spacingOffset = -(spacing / 2);
+            yPosition += spacing;
+            xStart += spacingOffset;
+            xPosition = xStart;
+        }
+
+        return positions;
     }
 }
