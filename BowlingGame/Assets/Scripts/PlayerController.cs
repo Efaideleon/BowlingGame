9122bbccef5523dbcab10b0962ba140a9eb0bf75
Efaideleon <efaideleon@gmail.com>
2024-07-22 10:50:21 -0700

    using unity's new input system

--- BowlingGame/Assets/Scripts/PlayerController.cs
+++ BowlingGame/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] Chargeable bowlingBall;
+    [SerializeField] private Chargeable bowlingBall;
+    private PlayerInputActions playerInputActions;
 
-    void Update()
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Charge.started += OnPlayerChargeStarted;
+        playerInputActions.Player.Charge.canceled += OnPlayerChargeCanceled;
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.Player.Charge.started -= OnPlayerChargeStarted;
+        playerInputActions.Player.Charge.canceled -= OnPlayerChargeCanceled;
+        playerInputActions.Player.Disable();
+    }
+
+    private void OnPlayerChargeStarted(InputAction.CallbackContext context)
     {
         if (bowlingBall != null)
         {
-            HandleInput();
+            bowlingBall.StartCharging();
         } 
         else 
         {
@@ -16,15 +37,16 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void HandleInput()
+    private void OnPlayerChargeCanceled(InputAction.CallbackContext context)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            bowlingBall.StartCharging();
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (bowlingBall != null)
         {
             bowlingBall.ReleaseCharge();
+        } 
+        else 
+        {
+            Debug.LogError("Error: bowlingBall has not been assigned to PlayerController");
         }
+
     }
 }
