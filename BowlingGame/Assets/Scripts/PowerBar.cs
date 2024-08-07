6bd12ce54116aa3b1b1132c4a5384cdc14690a8f
Abel De Leon <132021571+abeledl@users.noreply.github.com>
2024-08-07 16:07:15 -0700

    incomplete

--- BowlingGame/Assets/Scripts/PowerBar.cs
+++ BowlingGame/Assets/Scripts/PowerBar.cs
@@ -6,14 +6,25 @@ public class PowerBar : MonoBehaviour
     [SerializeField] private Image _fillImage;
     [SerializeField] private GameObject _powerBarContainer;
     [SerializeField] private BowlingBall _bowlingBall;
-
+    [SerializeField] private Material _material; // Ensure this is assigned in the Inspector
+    public float gradientHue;
     void Start()
     {
         _powerBarContainer.SetActive(false);
+        
+        if (_material != null)
+        {
+            _material.SetFloat("_GradientHue", gradientHue);
+        }
+        else
+        {
+            Debug.LogWarning("Material is not assigned");
+        }
     }
 
     void Update()
     {
+        _material.SetFloat("_GradientHue", gradientHue);
         if (_bowlingBall.ChargePercentage > 0)
         {
             SetPowerBarVisibility(true);
