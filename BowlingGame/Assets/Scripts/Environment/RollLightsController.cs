bc638138db35b16e6f939763daeea3904aa1270a
Efaideleon <efaideleon@gmail.com>
2024-10-31 20:01:28 -0700

    refactoring

---
+++ BowlingGame/Assets/Scripts/Environment/RollLightsController.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace environment
+{
+    [System.Serializable]
+    public class Lights 
+    {
+        [Tooltip("Lights for a lane")]
+        [SerializeField] public GameObject[] lights;
+    }
+    public class RollLightsController : MonoBehaviour
+    {
+        [SerializeField] private List<Lights[]> _tests;
+        [Tooltip("The current lane the player is in.")]
+        [SerializeField] private int _currentLaneIndex = 0;
+
+        [Header("Model")]
+        [Tooltip("Reference to the BowlingGame Model")]
+        [SerializeField] private BowlingGame _bowlingGame;
+
+        [Header("Configuration")]
+        [SerializeField] private float emissionIntensity = 1500;
+
+        [Header("Roll Lights")]
+        [SerializeField] private GameObject[] _laneOneLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneTwoLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneThreeLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneFourLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneFiveLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneSixLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneSevenLights = new GameObject[3];
+        [SerializeField] private GameObject[] _laneEightLights = new GameObject[3];
+
+        private List<GameObject[]> _lights = new(8);
+        private Color _initialColor;
+        private int _currentRollIndex => (int)_bowlingGame.CurrentFrame.CurrentRollNumber - 1;
+        private Material CurrentLight => GetCurrentLightMaterial();
+
+        void OnEnable()
+        {
+            _bowlingGame.OnRollCompleted += HandleLights;
+        }
+
+        void OnDisable()
+        {
+            _bowlingGame.OnRollCompleted -= HandleLights;
+        }
+
+        void Start()
+        {
+            _lights.Add(_laneOneLights);
+            _lights.Add(_laneTwoLights);
+            _lights.Add(_laneThreeLights);
+            _lights.Add(_laneFourLights);
+            _lights.Add(_laneFiveLights);
+            _lights.Add(_laneSixLights);
+            _lights.Add(_laneSevenLights);
+            _lights.Add(_laneEightLights);
+            _initialColor = _lights[0][0].GetComponent<MeshRenderer>().materials[1].color;
+            HandleLights();
+        }
+
+        private void HandleLights()
+        {
+            if (_currentRollIndex < 1)
+            {
+                ResetLights();
+            }
+
+            TurnOnCurrentLight();
+        }
+
+        private void ResetLights()
+        {
+            foreach(GameObject light in _lights[_currentLaneIndex])
+            {
+                Material lightMaterial = light.GetComponent<MeshRenderer>().materials[1];
+                ChangeMaterialBaseAndEmssiveColor(lightMaterial, _initialColor, _initialColor);
+            }
+        }
+
+        private void TurnOnCurrentLight()
+        {
+            ChangeMaterialBaseAndEmssiveColor(CurrentLight, Color.green, Color.green);
+        }
+
+        private void ChangeMaterialBaseAndEmssiveColor(Material material, Color emmisiveColor, Color baseColor)
+        {
+            // HDRP Update Emission color
+            material.EnableKeyword("_EMISSION");
+            material.SetColor("_EmissiveColor", baseColor * emissionIntensity);
+            material.SetColor("_BaseColor", emmisiveColor * emissionIntensity);
+        }
+
+        private Material GetCurrentLightMaterial()
+        {
+            var currentLights = _lights[_currentLaneIndex];
+            var currentLight = currentLights[_currentRollIndex];
+            return currentLight.GetComponent<MeshRenderer>().materials[1];
+        }
+    }
+}
